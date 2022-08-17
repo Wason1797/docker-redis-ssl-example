781a096bee7b0ab6c2a5b7f1eb953dc37aaddd90
Wladymir Brborich <wabrborich@hotmail.com>
2022-08-16 23:11:21 -0500

    Update test example to use a single connection client
    * with the original pool model redis in docker could not handle more than 160 concurrent asyncio requests
    
    * I found out that most likely each request was creating its own connection which lead to subpar performance
    
    * Using the context manager and the .client() method i managed to increase the limit to 100000 concurrent tasks using asyncio

--- aioredis_example.py
+++ aioredis_example.py
@@ -1,15 +1,43 @@
 import asyncio
 
-from aioredis import Redis
+from aioredis import from_url
+
+from time import perf_counter
 
 
 async def main():
-    redis = Redis(decode_responses=True, ssl=True, ssl_keyfile='./redis/tls/client.key',
-                  ssl_certfile='./redis/tls/client.crt', ssl_ca_certs='./redis/tls/ca.crt', password='password')
+    redis = from_url(url='rediss://localhost', decode_responses=True, ssl_keyfile='./redis/tls/client.key',
+                     ssl_certfile='./redis/tls/client.crt', ssl_ca_certs='./redis/tls/ca.crt',
+                     password='password')
 
-    await redis.set('key', 'value')
+    start = perf_counter()
+    size = 100000
 
-    print(await redis.get('key'))
+    async with redis.client() as client:
+
+        set_tasks = [client.set(f'key{number}', f'value{number}') for number in range(size)]
+        get_tasks = [client.get(f'key{number}') for number in range(size)]
+
+        end = perf_counter()
+
+        print(f'prepare took {end-start}s')
+
+        start = perf_counter()
+
+        await asyncio.gather(*set_tasks)
+
+        end = perf_counter()
+
+        print(f'setting took {end-start}s')
+
+        start = perf_counter()
+
+        results = await asyncio.gather(*get_tasks)
+
+        end = perf_counter()
+
+        print(f'getting took {end-start}s')
+        print('stored ', len(results), ' keys')
 
 
 if __name__ == '__main__':
